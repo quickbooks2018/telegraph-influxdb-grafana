7161f417a9a47d20e74d49650c55634c7dc8f883
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2021-12-12 16:39:46 +0500

    Update telegraph.sh

--- telegraf+influxdb-grafana/telegraph.sh
+++ telegraf+influxdb-grafana/telegraph.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 # https://hub.docker.com/_/telegraf
 
-HOST_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+HOSTNAME="Docker-Cloudgeeks-CA"
+HOSTIP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+
+SERVER="${HOSTNAME}"-"${HOSTIP}"
+
+export $HOSTNAME
+export $HOSTIP
+export $SERVER
 
 # Telegraf Setup
 cat << EOF > telegraf.conf
  [global_tags]
-
 [agent]
   interval = "60s"
   round_interval = true
@@ -16,47 +22,31 @@ cat << EOF > telegraf.conf
   flush_interval = "10s"
   flush_jitter = "0s"
   precision = ""
-  hostname = "$HOST_IP"
+  hostname = "$SERVER"
   omit_hostname = false
-
 [[outputs.influxdb]]
-  urls = ["http://monitoring.docker.local:8086"]
+  urls = ["http://192.168.195.185:8086"]
   database = "influx"
   timeout = "5s"
   username = "telegraf"
   password = "metricsmetricsmetricsmetrics"
-
-
 [[inputs.cpu]]
   percpu = true
   totalcpu = true
   collect_cpu_time = false
   report_active = false
-
-
 [[inputs.disk]]
   ignore_fs = ["tmpfs", "devtmpfs", "devfs", "iso9660", "overlay", "aufs", "squashfs"]
-
-
 [[inputs.docker]]  
-
  endpoint = "unix:///var/run/docker.sock"
-
 container_name_include = []
 container_name_exclude = []
-
 [[inputs.diskio]]
-
 [[inputs.kernel]]
-
 [[inputs.mem]]
-
 [[inputs.processes]]
-
 [[inputs.swap]]
-
 [[inputs.system]]
-
 EOF
 
 
